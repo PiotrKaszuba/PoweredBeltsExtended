a90d9081aa8bff6cc6afb3053ecbf08616662c77
PiotrKaszuba <piotrkaszuba1996@gmail.com>
2026-03-05 02:26:20 +0100

    add missing events

--- control.lua
+++ control.lua
@@ -22,6 +22,8 @@ script.on_event({
 	defines.events.on_built_entity,
 	defines.events.script_raised_built,
 	defines.events.script_raised_revive,
+	defines.events.on_entity_cloned,
+	defines.events.on_space_platform_built_entity
 }, event_handlers.on_built_entity)
 
 script.on_event({
@@ -29,6 +31,7 @@ script.on_event({
 	defines.events.on_robot_mined_entity,
 	defines.events.on_player_mined_entity,
 	defines.events.script_raised_destroy,
+	defines.events.on_space_platform_mined_entity,
 }, event_handlers.on_removed_entity)
 
 script.on_event(defines.events.on_tick, update_loop.on_tick)
